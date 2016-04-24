36dcb78952d1e20a60d0929c7e6e3539d0595ff2
Dmitry Shlagoff <wessmoke@gmail.com>
2016-04-24 21:04:13 +0700

    CLI.

--- src/main.rs
+++ src/main.rs
@@ -6,26 +6,71 @@ extern crate hyper;
 extern crate rustc_serialize;
 extern crate select;
 extern crate flate2;
+extern crate getopts;
+
+use std::env;
+
+use getopts::Options;
 
 use google::Google;
 use stackexchange::StackExchangeApi;
 
+
+fn print_usage(opts: Options) {
+    let brief = "Usage: how2-rs [options] query";
+    print!("{}", opts.usage(&brief));
+}
+
+
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let mut opts = Options::new();
+    opts.optopt("m", "max-answers", "maximum answers to retrieve", "COUNT");
+    opts.optflag("h", "help", "show usage");
+
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => { println!("{}", f.to_string()); return; }
+    };
+
+    if matches.opt_present("h") {
+        print_usage(opts);
+        return;
+    }
+
+    let max_answers_count;
+    if matches.opt_present("m") {
+        let count_cli = matches.opt_str("m");
+        match count_cli {
+            Some(count_str) => match count_str.parse::<usize>() {
+                Ok(count) => { max_answers_count = count; },
+                Err(_) => { println!("'m' option should be an integer."); return; }
+            },
+            None => { println!("Argument to option 'm' missing."); return; }
+        }
+    }
+    else {
+        max_answers_count = 5;
+    }
+
     let google = Google::default();
-    let links = google.google("python unittest skip base class");
+    let links = google.google(&matches.free.join(" "));
 
     let api = StackExchangeApi::new();
 
     let mut answers = Vec::new();
     for link in links.iter() {
         match api.answers(&link.link) {
-            Ok(ref mut link_answers) => answers.append(link_answers),
-            Err(_) => {}
+           Ok(ref mut link_answers) => answers.append(link_answers) ,
+           Err(_) => {}
         }
     }
     answers.sort_by_key(|a| -a.score);
+    let left_answers: Vec<_> = answers.iter().take(max_answers_count).collect();
 
-    for answer in answers.iter() {
-        println!("{}\n-----", answer);
+    for answer in left_answers.iter() {
+         println!("{}", answer);
+         println!("===-----===");
     }
 }
